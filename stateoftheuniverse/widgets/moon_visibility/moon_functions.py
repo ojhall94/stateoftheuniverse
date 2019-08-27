62fc76c2591315470e6fb735667df9d4d42b2bc5
Oliver Hall <ojh251@student.bham.ac.uk>
2019-08-27 18:42:51 +0100

    Added function to get moon illumination. Not yet commented

--- stateoftheuniverse/widgets/moon_visibility/moon_functions.py
+++ stateoftheuniverse/widgets/moon_visibility/moon_functions.py
@@ -2,3 +2,25 @@
 """
 A function to return moon phase & moon brightness.
 """
+
+import numpy as np
+import astroplan
+import astropy
+from astropy.time import Time
+
+def get_moon_illumination(time: datetime):
+    """ A function to return the moon illumination
+
+    I'll fill this in later
+    """
+
+    astropytime = Time(time)
+    try:
+        illumination = astroplan.moon.moon_illumination(astropytime)
+    except:
+        print("Something has gone wrong. Please raise an issue on GitHub")
+    return illumination
+
+# def get_moon_phase(illumination: float):
+#     """ A function to return the phase of the moon based on the illumination?
+#     """
