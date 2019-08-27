1a02d08bd14d4a08700ece899fd248ebe5c0e496
Eli Prater <eliprater@dhcp-10-241-184-7.cp.wireless.private.cam.ac.uk>
2019-08-27 18:56:32 +0100

    Angles calculated, need to get into coordinates reable by get_constellation

--- stateoftheuniverse/widgets/constillations/get_constillations.py
+++ stateoftheuniverse/widgets/constillations/get_constillations.py
@@ -1,6 +1,8 @@
+from astropy import units as u
 from astroplan import Observer, FixedTarget
-from astropy.coordinates import SkyCoord
+from astropy.coordinates import SkyCoord, AltAz, Angle, get_constellation
 from astropy.time import Time
+import numpy as np
 
 subaru = Observer.at_site('subaru')
 
@@ -9,4 +11,12 @@ vega = FixedTarget.from_name('Vega')
 
 time = Time('2019-08-27 12:00:00')
 
-print(subaru.target_is_up(time,altair))
+alt = Angle(np.arange(0,90,5), unit=u.deg)
+az = Angle(np.arange(0,395,5), unit=u.deg)
+
+#altaz = AltAz(az=az[0], alt=alt[0], location=subaru.location)
+altaz = SkyCoord(az=az[1]*u.degree, alt=alt[1]*u.degree, frame='altaz') 
+print(subaru.location)
+print(type(subaru))
+print(altaz)
+print(get_constellation(altaz))
