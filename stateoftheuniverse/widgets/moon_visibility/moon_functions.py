07f3899ec82184b5fdedc29c7fc2668250e6f913
Oliver Hall <ojh251@student.bham.ac.uk>
2019-08-28 16:00:53 +0100

    Figured out how to calculate moon waxing or waning

--- stateoftheuniverse/widgets/moon_visibility/moon_functions.py
+++ stateoftheuniverse/widgets/moon_visibility/moon_functions.py
@@ -7,6 +7,9 @@ import numpy as np
 import astroplan
 import astropy
 from astropy.time import Time
+import matplotlib.pyplot as plt
+from astropy.coordinates import get_moon, get_sun
+import datetime
 
 def get_moon_illumination(time: datetime):
     """ A function to return the moon illumination
@@ -21,6 +24,38 @@ def get_moon_illumination(time: datetime):
         print("Something has gone wrong. Please raise an issue on GitHub")
     return illumination
 
-# def get_moon_phase(illumination: float):
-#     """ A function to return the phase of the moon based on the illumination?
-#     """
+def get_moon_phase(illumination: float):
+    """ A function to return the phase of the moon based on the illumination?
+    """
+
+if __name__ == "__main__":
+    i = []
+    ras = []
+    rasuns = []
+    state = [] #0 waxing 1 waning
+    for  d in range(30):
+        date = datetime.datetime(2019, 1, d+1)
+        time = Time(date)
+        moon = get_moon(time)
+        sun = get_sun(time)
+        ras.append(moon.ra.value)
+        rasuns.append(sun.ra.value)
+
+        ra = moon.ra.value
+        rasun = sun.ra.value
+        if ra > rasun:
+            state.append(0)
+        elif (ra < rasun-180.):
+            state.append(0)
+        elif (ra > rasun-180) & (ra < rasun):
+            state.append(1)
+        elif (ra == rasun):
+            state.append(2)
+        elif (ra == rasun-180.):
+            state.append(3)
+
+
+    plt.scatter(range(30), ras, c=state)
+    plt.axhline(sun.ra.value)
+    plt.axhline(sun.ra.value-180.)
+    plt.show()
