854f386986543c86e96132cacc7aa006af0223da
astromariarosa <mariarosamarinelli@gmail.com>
2019-08-28 10:15:46 -0400

    Fixed code so it works with Booleans args

--- get_asteroidcount.py
+++ get_asteroidcount.py
@@ -1,41 +1,40 @@
-def get_NEA_count(by_method: bool = True,
-                        by_class: bool = True) -> dict:
+from astroquery.mpc import MPC
+
+def get_NEA_count(hazard: bool = True,
+                        large: bool = True) -> dict:
     """
-    Get the current total number of confirmed exoplanets from the NASA
-    Exoplanet Archive. Additionally, also get the number of exoplanets
-    grouped by detection method or by planet class (if desired).
+    Get the current total number of near-Earth asteroids from the IAU
+    Minor Planet Center. Additionally, get the number of asteroids that
+    identified as potentially hazardous and larger than 1 km.
+
     Args:
-        by_method: Whether or not to also return the exoplanet count
-            grouped by detection method.
-        by_class: Whether or not to also return the exoplanet count
-            grouped by (approximate) planet class.
+        hazard: Returning the number of potentially hazardous
+            near-Earth asteroids.
+        large: Returning the number of near-Earth asteroids that
+            are larger than 1 km wide.
     Returns:
-        A dictionary with keys `{"total", "by_method", "by_class"}`
-        which holds the respective count of exoplanets.
+        A dictionary with keys `{"hazard", "large", "total"}`
+        which holds the respective count of asteroids.
     """
+    asteroid_count = dict()
 
     # Get all potentially hazardous asteroids from MPC
-    PHA = MPC.query_objects('asteroid', pha=1)
+    PHA = len(MPC.query_objects('asteroid', pha=1))
+    asteroid_count['hazard'] = PHA
 
     # Get all non-potentially hazardous asteroids greater than 1 km wide from MPC
-    NEA_1km = MPC.query_objects('asteroid', pha=0, km_neo=1)
+    NEA_1km = len(MPC.query_objects('asteroid', pha=0, km_neo=1))
+    asteroid_count['large'] = NEA_1km
 
     # Get non-potentially hazardous asteroids smaller than 1 km from MPC
-    # (do it in stages since there is a limit to the number of objects that
-    # query_objects can call)
-    neo0 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=0)
-    neo1 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=1)
-    neo2 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=2)
-    neo3 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=3)
-    neo4 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=4)
-    neo5 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=5)
-    neo6 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=6)
-    neo7 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=7)
-    neo8 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=8)
-    neo9 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=9)
-    neo10 = MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=10)
-    NEA_count = len(neo0)+len(neo1)+len(neo2)+len(neo3)+len(neo4)+len(neo5)+len(neo6)+len(neo7)+len(neo8)+len(neo9)+len(neo10)
+    neos = [MPC.query_objects('asteroid', pha=0, km_neo=0, neo=1, orbit_type=orbtype) for orbtype in range(10)]
+    NEA_count = sum(len(neo) for neo in neos)
+    asteroid_count['total'] = NEA_count
 
-    total_NEA_count = PHA + NEA_1km + NEA_count
+    print("We have discovered", asteroid_count['total'], "asteroids.")
 
-    return total_NEA_count
+    if large:
+        print("Of those, there are", asteroid_count['large'], "near-Earth asteroids larger than 1 km wide.")
+
+    if hazard:
+        print("Currently, there are", asteroid_count['hazard'], "near-Earth asteroids that are classified as potentially hazardous.")
