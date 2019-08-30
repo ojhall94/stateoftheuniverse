a7b95ce071953b2cc499fd0cfb19f90e8e6c4a2f
Timothy Gebhard <mail@timothygebhard.de>
2019-08-30 11:08:38 +0100

    Update docstring to include example of how to use the stringdecorator

--- stateoftheuniverse/widgets/utils.py
+++ stateoftheuniverse/widgets/utils.py
@@ -18,6 +18,26 @@ def stringdecorator(function: Callable) -> Callable:
     Decorator function which can be used to easily add dashed lines,
     name, etc. to the `get_string()` method of a widget.
 
+    Example:
+        In you widget, the signature the the get_string() method
+        should look like the following:
+        ```
+        @stringdecorator
+        def get_string(self):
+            ...
+            return string
+        ```
+        This will make the output of get_string() to look like this:
+        ```
+        ----------------------------------------------------------------
+            WIDGET NAME (given by self.name in widget constructor)
+        ----------------------------------------------------------------
+
+        Widget output (i.e., the original string from get_string()).
+
+        ----------------------------------------------------------------
+        ```
+
     Args:
         function: The function to be decorated. Generally, this will
             be the `get_string()` method of a widget.
