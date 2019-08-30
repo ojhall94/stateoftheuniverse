0b9a538976ef09f902c4ec0ddb53fe9ab3833f59
Ania Kapuścińska <ania0102@gmail.com>
2019-08-30 14:48:15 +0100

    Add main.py script calling all the widgets

---
+++ stateoftheuniverse/main.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from widgets import (
+    EphemBodies,
+    MoonPhaseWidget,
+    ConstellationsWidget,
+    ExoplanetCount,
+    AsteroidCount,
+    AstronomerBirthdays
+)
+
+
+def main():
+    lat = 52.2
+    lon = 0.11667
+    dt = datetime.utcnow()
+
+    for widget_class in [
+        EphemBodies,
+        MoonPhaseWidget,
+        ConstellationsWidget,
+        ExoplanetCount,
+        AsteroidCount,
+        AstronomerBirthdays
+    ]:
+        widget = widget_class(lon, lat, dt)
+        widget.get_data()
+        print(widget.get_string())
+
+
+if __name__ == '__main__':
+    main()
